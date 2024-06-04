d539649927e336e45ae3f61803365795f74180fa
Артур Вольнов <Volnovae@aquaros.com>
2024-06-04 16:17:08 +0500

    0.1.1

--- ssh_tunnel/connecting.py
+++ ssh_tunnel/connecting.py
@@ -1,21 +1,22 @@
 import asyncssh
 import os
 
-from .transfer import transfer_file
-from archiving.archiving import path_to_zip
-
 
 IP_SERVER = os.getenv('IP_SERVER')
 PORT_SSH = int(os.getenv('PORT_SSH'))  # Замените на ваш порт
 USER = os.getenv('USER')
-REMOTE_PATH: str = '/home/expert/Desktop/'
+PASSWORD = os.getenv('PASSWORD')
+DISK = os.getenv('DISK')
+ARCHIVE_NAME = os.getenv('ARCHIVE_NAME')
+PATH = os.getenv('PATH')
 
 
 async def connect_ssh():
     try:
-        # Подключение к серверу
-        async with asyncssh.connect(host=IP_SERVER, port=PORT_SSH, username=USER) as conn:
-            await transfer_file(conn=conn, local_path=path_to_zip, remote_path=REMOTE_PATH)
-            return True
+        async with asyncssh.connect(host=IP_SERVER, port=PORT_SSH, username=USER, password=PASSWORD) as conn:
+            result = await conn.run(f'{DISK}', check=True)
+            print(result)
+            result = await conn.run(f'7z a {ARCHIVE_NAME} {PATH}', check=True)
+            print(result)
     except (OSError, asyncssh.Error) as exc:
         return f'Ошибка подключения: {exc}'
