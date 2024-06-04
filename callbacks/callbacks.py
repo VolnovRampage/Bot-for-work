82d19aac1b3065b602e7b69b3308815c55f623f2
Артур Вольнов <Volnovae@aquaros.com>
2024-06-04 16:17:08 +0500

    0.1.1

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -1,7 +1,6 @@
 import os
 
 from aiogram import F, Router, types
-from archiving.archiving import create_archive
 from ssh_tunnel.connecting import connect_ssh
 
 
@@ -14,13 +13,6 @@ callback_router = Router()
 @callback_router.callback_query(F.data == 'backup_sql')
 async def backup(call: types.CallbackQuery):
     if call.from_user.id in ADMINS:
-        await call.message.edit_text('Начинаю архивировать папку.')
-        await create_archive()
-        await call.message.edit_text('Архивация закончена.\nОтправляю архив на сервер.')
-        sending: str | bool = await connect_ssh()
-        if not isinstance(sending, str):
-            await call.message.edit_text('Файл успешно отправлен')
-        else:
-            await call.message.edit_text(text=sending)
+        await connect_ssh()
     else:
         await call.message.edit_text('У вас нет прав на это')
