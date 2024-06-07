288f5d3355e2be19c6c97c4c2610282abe8d844b
Артур Вольнов <Volnovae@aquaros.com>
2024-06-07 14:45:38 +0500

    test

--- ssh_tunnel/connecting.py
+++ ssh_tunnel/connecting.py
@@ -1,22 +1,21 @@
 import asyncssh
+import asyncio
 import os
+from dotenv import load_dotenv, find_dotenv
 
-
+load_dotenv(find_dotenv())
 IP_SERVER = os.getenv('IP_SERVER')
 PORT_SSH = int(os.getenv('PORT_SSH'))  # Замените на ваш порт
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
-DISK = os.getenv('DISK')
-ARCHIVE_NAME = os.getenv('ARCHIVE_NAME')
-PATH = os.getenv('PATH')
+# DESTINATIO = os.getenv('DESTINATIO')
+# ARCHIVE_NAME = os.getenv('ARCHIVE_NAME')
+# PATH = os.getenv('PATH')
 
 
-async def connect_ssh():
-    try:
-        async with asyncssh.connect(host=IP_SERVER, port=PORT_SSH, username=USER, password=PASSWORD) as conn:
-            result = await conn.run(f'{DISK}', check=True)
-            print(result)
-            result = await conn.run(f'7z a {ARCHIVE_NAME} {PATH}', check=True)
-            print(result)
-    except (OSError, asyncssh.Error) as exc:
-        return f'Ошибка подключения: {exc}'
+async def connect_via_ssh():
+    async with asyncssh.connect(host=IP_SERVER, port=PORT_SSH, username=USER) as conn:
+        result = await conn.run('ls -ls')
+        print(result)
+
+asyncio.run(connect_via_ssh())
