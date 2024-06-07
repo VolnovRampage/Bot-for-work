3436b1a73efd4e22439ed84ecdfce0a45b141011
Артур Вольнов <Volnovae@aquaros.com>
2024-06-07 17:44:05 +0500

    add connection

--- ssh_tunnel/connecting.py
+++ ssh_tunnel/connecting.py
@@ -4,18 +4,36 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+# Получение значений переменных окружения
 IP_SERVER = os.getenv('IP_SERVER')
-PORT_SSH = int(os.getenv('PORT_SSH'))  # Замените на ваш порт
+PORT_SSH = os.getenv('PORT_SSH')
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
-# DESTINATIO = os.getenv('DESTINATIO')
-# ARCHIVE_NAME = os.getenv('ARCHIVE_NAME')
-# PATH = os.getenv('PATH')
 
+# Отладочная печать переменных окружения
+print(f"IP_SERVER: {IP_SERVER}")
+print(f"PORT_SSH: {PORT_SSH}")
+print(f"USER: {USER}")
+print(f"PASSWORD: {PASSWORD}")
 
 async def connect_via_ssh():
-    async with asyncssh.connect(host=IP_SERVER, port=PORT_SSH, username=USER) as conn:
-        result = await conn.run('ls -ls')
-        print(result)
+    try:
+        print("Попытка подключения к серверу...")
+        async with asyncssh.connect(host=IP_SERVER, port=int(PORT_SSH), username=USER, password=PASSWORD) as conn:
+            print("Подключение успешно. Выполнение команды...")
+
+            # Команда для создания папки на рабочем столе Windows
+            create_folder_command = r'mkdir "c:\Users\karpovicha\Desktop\TEST"'  # Используем сырые строки (raw strings), чтобы избежать экранирования обратных слешей
+
+            # Выполнение команды для создания папки
+            result = await conn.run(create_folder_command, check=True)
+            print("Команда выполнена успешно. Результат:")
+            print(result.stdout)
+    except asyncssh.PermissionDenied:
+        print("Permission denied: Проверьте правильность имени пользователя и пароля")
+    except asyncssh.Error as e:
+        print(f"Ошибка SSH: {e}")
+    except Exception as e:
+        print(f"Ошибка подключения: {e}")
 
 asyncio.run(connect_via_ssh())
