2d388c470cd249bff7fa5541540287b66be45ecd
Артур Вольнов <Volnovae@aquaros.com>
2024-06-13 15:40:55 +0500

    0.5.0

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -3,10 +3,9 @@ import time
 
 from datetime import datetime
 from aiogram import F, Router, types
-from ssh_tunnel.connecting import connect_via_ssh, measurement_file
+from ssh_tunnel.connecting import connect_via_ssh, get_file_size
 
-
-
+from keyboards.inline_keyboards import inline_kb
 
 ADMINS: list = [int(id) for id in os.getenv("ADMINS").split(",")]
 
@@ -17,25 +16,29 @@ def get_date():
     return datetime.now().strftime("%H:%M:%S  |  %d/%m/%Y")
 
 
-@callback_router.callback_query(F.data == 'backup_sql')
+@callback_router.callback_query(F.data == "backup_sql")
 async def backup(call: types.CallbackQuery):
     if call.from_user.id in ADMINS:
-        await call.message.edit_text(text='Запускаю BackUp')
+        initial_text = "Начинается BackUp sql файлов"
+        if call.message.text != initial_text:
+            await call.message.edit_text(text=initial_text)
         start_time = time.time()
         result = await connect_via_ssh()
         if result is True:
-            spent_time = (time.time() - start_time) / 100
+            spent_time = (time.time() - start_time) / 60
             current_date = get_date()
-            file_size = measurement_file()
-            await call.message.edit_text(
-                text='<b>BackUp выполнен.</b>\n' \
-                    f'<b>Затрачено времени:</b>  {spent_time:.2f} минут.\n' \
-                    f'<b>Передано по проводу:</b>  {file_size:.2f} Гб.\n' \
-                    f'<b>Время и дата:</b>  {current_date}.'
-            )
+            file_size = get_file_size()
+            await call.message.delete()
+            text: str = "<b>BackUp выполнен.</b>\n" \
+                f"<b>Затрачено времени:</b>  {spent_time:.2f} минут.\n" \
+                f"<b>Передано по проводу:</b>  {file_size:.3f} Гб.\n" \
+                f"<b>Время и дата:</b>  {current_date}."
+            await call.message.answer(text=text, reply_markup=inline_kb.as_markup())
         else:
             current_date = get_date()
-            await call.message.edit_text(text=f'{result}\n<b>Время и дата:</b>  {current_date}')
+            await call.message.edit_text(
+                text=f"{result}\n<b>Время и дата:</b>  {current_date}"
+            )
 
     else:
-        await call.message.edit_text('У вас нет прав на это')
+        await call.message.edit_text("У вас нет прав на это")
--- ssh_tunnel/connecting.py
+++ ssh_tunnel/connecting.py
@@ -1,41 +1,54 @@
 import asyncssh
 import os
+import asyncio
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 # Получение значений переменных окружения
-IP_SERVER = os.getenv('IP_SERVER')
-PORT_SSH = os.getenv('PORT_SSH')
-USER = os.getenv('USER')
-PASSWORD = os.getenv('PASSWORD')
-PATH_TO_SOURCE = os.getenv('PATH_TO_SOURCE')
-PATH_TO_ARCHIVE = os.getenv('PATH_TO_ARCHIVE')
-PATH_TO_DESTINATION = os.getenv('PATH_TO_DESTINATION')
+IP_SERVER = os.getenv("IP_SERVER")
+PORT_SSH = os.getenv("PORT_SSH")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+PATH_TO_SOURCE = os.getenv("PATH_TO_SOURCE")
+PATH_TO_ARCHIVE = os.getenv("PATH_TO_ARCHIVE")
+PATH_TO_DESTINATION = os.getenv("PATH_TO_DESTINATION")
 
 
-async def create_archive(conn:asyncssh):
-    create_archive = f'7z a {PATH_TO_ARCHIVE} {PATH_TO_SOURCE}'
+async def create_archive(conn: asyncssh.SSHClientConnection):
+    create_archive = f"7z a {PATH_TO_ARCHIVE} {PATH_TO_SOURCE}"
     await conn.run(create_archive, check=True)
     return conn
 
 
-async def send_archive(conn:asyncssh):
+async def send_archive(conn: asyncssh.SSHClientConnection):
     async with conn.start_sftp_client() as sftp:
-        remote_path = PATH_TO_ARCHIVE.replace('\\', '/')
-        local_path = os.path.join(PATH_TO_DESTINATION, os.path.basename(PATH_TO_ARCHIVE)).replace('\\', '/')
+        remote_path = PATH_TO_ARCHIVE.replace("\\", "/")
+        local_path = os.path.join(
+            PATH_TO_DESTINATION, os.path.basename(PATH_TO_ARCHIVE)
+        ).replace("\\", "/")
         await sftp.get(remote_path, local_path)
 
 
-def measurement_file():
-    file_size = os.path.getsize(os.path.join(PATH_TO_DESTINATION, os.path.basename(PATH_TO_ARCHIVE)))
-    return file_size / (1024 ** 3)
+def get_file_size():
+    file_size = os.path.getsize(
+        os.path.join(PATH_TO_DESTINATION, os.path.basename(PATH_TO_ARCHIVE))
+    )
+    return file_size / (1024**3)
+
+
+async def remove_archive(conn: asyncssh.SSHClientConnection):
+    await conn.run(f"del {PATH_TO_ARCHIVE}")
+    return True
 
 
 async def connect_via_ssh():
     try:
-        async with asyncssh.connect(host=IP_SERVER, port=int(PORT_SSH), username=USER, password=PASSWORD) as conn:
+        async with asyncssh.connect(
+            host=IP_SERVER, port=int(PORT_SSH), username=USER, password=PASSWORD
+        ) as conn:
             await create_archive(conn=conn)
             await send_archive(conn=conn)
+            await remove_archive(conn=conn)
             return True
     except asyncssh.PermissionDenied:
         return "<b>Permission denied:</b> Проверьте правильность имени пользователя и пароля"
@@ -43,3 +56,6 @@ async def connect_via_ssh():
         return f"<b>Ошибка SSH:</b> {e}"
     except Exception as e:
         return f"<b>Ошибка подключения:</b> {e}"
+
+
+asyncio.run(connect_via_ssh())
