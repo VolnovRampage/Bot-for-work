cdab781ecaf5f7fdec97097f5dc24c2121dd1fea
Artur Volnov <fristail95@gmail.com>
2024-06-27 16:11:12 +0500

    1.2.0

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -1,3 +1,4 @@
+import asyncio
 from aiogram import F, Router, types
 from ssh_tunnel.connecting import connect_via_ssh
 
@@ -8,29 +9,51 @@ from data.load_data import ADMINS
 
 
 callback_router = Router()
+backup_lock: bool = False
 
 
 async def backup():
-    result = await connect_via_ssh()
-    mode: str = ''
-    for index, text in enumerate(result):
-        if index == 0:
-            mode = 'w'
-        else:
-            mode = 'a'
-        await write_log(text=text, mode=mode)
-    return result
+    if not backup_lock:
+        set_backup_lock()
+        result = await connect_via_ssh()
+        mode: str = ''
+        for index, text in enumerate(result):
+            if index == 0:
+                mode = 'w'
+            else:
+                mode = 'a'
+            await write_log(text=text, mode=mode)
+        set_backup_lock()
+        return result
+
+
+def set_backup_lock():
+    global backup_lock
+    if backup_lock:
+        backup_lock = False
+    else:
+        backup_lock = True
+
+
+async def start_backup(call: types.CallbackQuery):
+    if backup_lock:
+        await call.message.edit_text("Backup уже запущен ⚠️")
+    else:
+        await call.message.edit_text(text="Начинается BackUp sql файлов 🔄")
+        try:
+            text = await backup()
+            text = '\n'.join(text)
+            await call.message.edit_text(text=text, reply_markup=inline_kb.as_markup())
+        except Exception as e:
+            await call.message.edit_text(text=f"Ошибка во время backup: {e}")
 
 
 @callback_router.callback_query(F.data == "backup_sql")
 async def backup_sql(call: types.CallbackQuery):
     if call.from_user.id in ADMINS:
-        await call.message.edit_text(text="Начинается BackUp sql файлов")
-        text = await backup()
-        text = '\n'.join(text)
-        await call.message.edit_text(text=text, reply_markup=inline_kb.as_markup())
+        asyncio.create_task(start_backup(call))
     else:
-        await call.message.edit_text("У вас нет прав на это.")
+        await call.message.edit_text("У вас нет прав на это ⛔️")
 
 
 @callback_router.callback_query(F.data == "info")
